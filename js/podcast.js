d4aa4b5bfb9ea1eeab82834043cc6e04dc80f171
Jake Howard <bsidedrums@Jake-Howards-MacBook-Pro.local>
2019-01-24 13:49:45 -0800

    added hamburger function

--- js/podcast.js
+++ js/podcast.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
 
+	$(".hamburger").on('click', function() {
+		
+		let $nav = $('nav');		
+
+		$(this).toggleClass("open");
+		if ($nav.hasClass('open-menu')) {
+			$nav.removeClass('open-menu');
+		} else {
+			$nav.addClass('open-menu');
+		}
+	 });
+
 	$(window).scroll(function() {
 		if ($(document).scrollTop() > 600) {
 			$('header ul li').fadeOut(500);
 		} else {
 			$('header ul li').fadeIn(500);
+		} 
+
+		if($(document).scrollTop() > 600) {
+			$(".hamburger span").addClass('scroll');
+		} else {
+			$(".hamburger span").removeClass('scroll');
 		}
 	});
 
