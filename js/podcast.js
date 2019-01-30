c548db90ff3567cffaac1134287037bab3a51ccc
Jake Howard <bsidedrums@Jake-Howards-MacBook-Pro.local>
2019-01-30 14:23:47 -0800

    added AJAX to js file

--- js/podcast.js
+++ js/podcast.js
@@ -30,4 +30,26 @@ $(document).ready(function() {
 		}
 	});
 
+	$('button').on('click', function() {
+		$('button').removeClass('selected');
+		$(this).addClass('selected');
+		var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+		var animal = $(this).text();
+		var flickrOptions = {
+			tags: animal,
+			format: "json"
+		};
+		function displayPhotos(data) {
+			var photoHTML = '<ul>';
+			$.each(data.items, function(i, photo) {
+				photoHTML += '<li>';
+				photoHTML += '<a href="' + photo.link + '">';
+				photoHTML += '<img src"' + photo.media.m + '"></a></li>';
+			});
+			photoHTML += '</ul>';
+			$('#photos').html(photoHTML);
+		}
+		$.getJSON(flickrAPI, flickrOptions, displayPhotos);
+	});
+
 });				
